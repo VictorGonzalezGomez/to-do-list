4a835848fcdb24d765c610d8e8588fac3bad8a5d
victor gonzalez <v.gonzalezgomez.vg@gmail.com>
2022-07-30 21:17:55 -0400

    dev adding task and deleting task

--- assets/js/dataAssignment.js
+++ assets/js/dataAssignment.js
@@ -1,14 +1,15 @@
-export const dataAssignments = [
+ export const dataAssignments = [
     {
         id:"1",
         name:"ejemplo de tarea 1"
     },
     {
         id:"2",
-        name:" ejemplo e tarea 2"
+        name:" ejemplo de tarea 2"
     },
     {
         id:"3",
         name:"ejemplo de tarea 3"
     }
 ]
+// export default dataAssignments
--- assets/js/main.js
+++ assets/js/main.js
@@ -6,12 +6,22 @@ const containerAssignments = document.getElementById("assignmentsContainer");
 const totalAssingment = document.getElementById("totalAssignmets");
 const totalFinishAssignments = document.getElementById("totalFinishAssignments");
 /**********  generar id **********/
-const idGenerator = () => Math.floor(Math.random() * Date.now());
+function idGenerator() {
+  return parseInt((Math.random() * new Date().getUTCMilliseconds()));
+} 
+function deleteAssignment(id) {
+  // let deletedAssignments = document.getElementById(`${id}`)
+  // deletedAssignments.remove(deletedAssignments);
+  console.log(id);
+ }
 /**********  crear plantilla a incertar en html **********/
 function renderAssignment(id, name) {
-  return  `<tr>
+  return  `<tr id="${id}" class="assignments" >
   <th>${id}</th>
-  <th>${name} <input type="checkbox" name="" id="">    <button> x </button></th>
+  <th>${name} 
+    <input type="checkbox" name="" id="" value="first_checkbox">    
+    <button onclick="deleteAssignment(${id})"> x </button>
+  </th>
 </tr>`
 }
 /**********  crear funcion para tomar los elementos por default del arreglo y enviarlos a la funcion addAssignment **********/
@@ -29,6 +39,8 @@ totalAssingmets(totalAssingment);
 function newDataAssingment(id, name) {
   const newAssingment = {id:id, name:name};
  dataAssignments.push(newAssingment);
+ 
+ containerAssignments.innerHTML += renderAssignment (newAssingment.id, newAssingment.name)
 }
 /**********  crear funcion para contar e insertar el total de tareas realizadas *********/
 function totalAssingmets(totalAssingment) {
@@ -42,7 +54,15 @@ function totalAssingmets(totalAssingment) {
 /**********  escuchar el boton agregar y realizar la operacion de insertar la nueva tarea en html **********/
 buttonAdd.addEventListener("click", () =>{ 
   let assingmentName = document.getElementById("nameAssignment").value;
-
+  newDataAssingment(idGenerator(), assingmentName);
+  totalAssingmets(totalAssingment);
 });
 /**********  crear funcion para eliminar un la tarea clickeada en el arreglo **********/
+
 /**********  escuchar de manera dinamica el boton para eliminar de manera correta la tarea seleccionada **********/
+//  document.querySelectorAll("button").forEach((element) => {
+//   element.addEventListener("click", (e) => {
+
+//     deleteAssignment();
+//   });
+// });  
